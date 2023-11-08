e3f8b2a3868c7c5ec286d473e29a60dbfa83092a
Aleksandr <87315796+EAMstudio@users.noreply.github.com>
2023-11-08 16:12:10 +0400

    Test

--- Count words.py
+++ Count words.py
@@ -12,7 +12,7 @@ words = re.findall(r'\w+', text.lower())
 # Подсчет частотности слов
 word_counts = Counter(words)
 
-# Вывод топ 30 слов и их частотности
+# Вывод топ 30 слов и их частотности.
 print("Word - Count Mention")
 for word, count in sorted(word_counts.items(), key=lambda x: x[1], reverse=True)[:30]:
     print(f"{word} - {count}")
