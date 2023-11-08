b2aec019b9b42a018c194e1ffde1ae434a77c5f4
Aleksandr <87315796+EAMstudio@users.noreply.github.com>
2023-11-08 16:33:13 +0400

    Added a function to analyze text using a 'def' procedure
    In this commit, I introduced a Python function named 'analyze_text'. This function tokenizes input text using regular expressions, calculates word frequencies, and prints the top 30 words and their counts. It allows for easy text analysis and can be used with different texts. Simply replace the content in the 'text' variable and call the 'analyze_text(text)' function to analyze the new text.

--- Count words.py
+++ Count words.py
@@ -1,18 +1,26 @@
 from collections import Counter
 import re
 
-# Ваш текст (замените эту строку на ваш текст)
+def analyze_text(text):
+    # Split the text into words using a regular expression
+    words = re.findall(r'\w+', text.lower())
+
+    # Count word frequency
+    word_counts = Counter(words)
+
+    # Print the top 30 words and their frequencies
+    print("Word - Count Mention")
+    for word, count in sorted(word_counts.items(), key=lambda x: x[1], reverse=True)[:30]:
+        print(f"{word} - {count}")
+
+# Text here
 text = """
-Ваш текст здесь
+Александр Пушкин
+Я помню чудное мгновенье:
+Передо мной явилась ты,
+Как мимолетное виденье,
+Как гений чистой красоты.
 """
 
-# Разделите текст на слова с помощью регулярного выражения
-words = re.findall(r'\w+', text.lower())
-
-# Подсчет частотности слов
-word_counts = Counter(words)
-
-# Вывод топ 30 слов и их частотности.
-print("Word - Count Mention")
-for word, count in sorted(word_counts.items(), key=lambda x: x[1], reverse=True)[:30]:
-    print(f"{word} - {count}")
+# Call the function with your text
+analyze_text(text)
